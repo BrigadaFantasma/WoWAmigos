b9bf68b16e192cd774e5f8524e095d7f324026b5
BrigadaFantasma <wow.brigadafantasma@gmail.com>
2025-05-24 14:44:58 -0400

    reinicio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://BrigadaFantasma.github.io',
+    base: 'WoWAmigos',
+    vite: {
+        plugins: [tailwindcss()],
+    }
+});
